3e8d4387a4f130cdad457db87942951d1d9a3649
Adriano <adriano@arkcode.com.br>
2023-03-13 17:23:25 -0300

    Models & Seeds

---
+++ app/Models/ProfileSubType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProfileSubType extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'profile_id',
+        'name',
+        'description',
+        'status',
+        'dashboard',
+    ];
+
+    protected $table = 'profile_subtypes';
+}
